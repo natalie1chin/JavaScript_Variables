ef60c6d72ec0a4c1a8311faefe2fb68a5a4407ba
Natalie Chin <natalie.h.chin@gmail.com>
2023-01-02 12:14:40 -0800

    finished hw

--- app.js
+++ app.js
@@ -4,7 +4,31 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Natalie";
+let lastName="Chin";
+var age=24;
 
 // Exercise 2
+lastName="Chin";
+age=24;
+
 
 // Exercise 3
+let language ="JavaScript";
+let createdYear=1995;
+let isCaseSensitive=true;
+
+// Exercise 4
+let price =19.99;
+let isOnSAle = false;
+let salePercentage=15;
+let stock = 0;
+let inStock=false;
+let selectedSize="M";
+
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark=456;
+let hasRead=true;
